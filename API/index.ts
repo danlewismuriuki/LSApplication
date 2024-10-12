05235dcb1de3abccba4874b9a845d591424e74f3
lsd <danlewismuriuki2@gmail.com>
2024-10-12 23:01:15 +0300

    feat: Update index.ts and package.json dependencies
    - Modified index.ts to include CORS middleware.
    - Updated package.json and package-lock.json to reflect new dependencies.

--- API/index.ts
+++ API/index.ts
@@ -1,9 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json())
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World Today');
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Server is running on localhost ${PORT}`)
+});
